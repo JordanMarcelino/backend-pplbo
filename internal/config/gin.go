1eb82417af9d1e6534a9e27d4167adec4c29df4a
JordanMarcelino <jhordan.marcelino@gmail.com>
2024-05-17 16:05:03 +0700

    feat: add cors middleware

--- internal/config/gin.go
+++ internal/config/gin.go
@@ -1,10 +1,14 @@
 package config
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func NewGin(config *Config) *gin.Engine {
 	app := gin.Default()
 	app.Use(NewErrorHandler())
+	app.Use(cors.Default())
 
 	if !config.Debug {
 		gin.SetMode(gin.ReleaseMode)
